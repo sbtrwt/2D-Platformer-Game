03a658ec1832f89d4dab59769da707a0884e3b07
Shobhit Rawat <sbtrwt.se@gmail.com>
2023-10-03 22:45:08 +0530

    Accept horizontal input from user and flip player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Player controller awake");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        //Control user by inputs
+        float speed = Input.GetAxisRaw("Horizontal");
+        if(speed > 0)
+            Debug.Log(speed);
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+
+        Vector3 scale = transform.localScale;
+        if (speed < 0) {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else {
+            scale.x = Mathf.Abs(scale.x);
+        }
+
+        transform.localScale = scale;
     }
 }

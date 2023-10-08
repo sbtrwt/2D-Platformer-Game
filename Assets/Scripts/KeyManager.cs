68941d4ac46a6eeade5cd079c4a84b2f776f3553
Shobhit Rawat <sbtrwt.se@gmail.com>
2023-10-08 17:43:27 +0530

    Added rotating animations of key

--- Assets/Scripts/KeyManager.cs
+++ Assets/Scripts/KeyManager.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class KeyManager : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Inside collision Key");
         PlayerController player = collision.gameObject.GetComponent<PlayerController>();
         if (player) {
             player.PickUpKey();
+            animator.SetBool("IsHide", true);
             Destroy(gameObject);
         }
 

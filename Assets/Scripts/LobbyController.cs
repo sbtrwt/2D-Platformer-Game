954cddd89e92391a477362bb45334f2713ba7f82
Shobhit Rawat <sbtrwt.se@gmail.com>
2023-10-09 22:43:17 +0530

    lobby scene added

---
+++ Assets/Scripts/LobbyController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LobbyController : MonoBehaviour
+{
+    [SerializeField] private Button buttonPlay;
+    [SerializeField] private Button buttonQuit;
+    private void Start()
+    {
+        buttonPlay.onClick.AddListener(PlayGame);
+        buttonQuit.onClick.AddListener(QuitGame);
+    }
+
+    private void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+    private void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
